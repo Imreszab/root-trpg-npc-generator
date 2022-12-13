0415a23e807723d72bbe66e9880902b7dd3205da
Imreszab <szabimi12@gmail.com>
2022-12-13 10:54:20 +0100

    add DbContext

---
+++ RootNpcGenerator/RootNpcBackend/Data/RootContext.cs
@@ -0,0 +1,20 @@
+﻿using RootNpcBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RootNpcBackend.Data
+{
+    public class RootContext : DbContext
+    {
+        public RootContext(DbContextOptions<RootContext> options) : base(options)
+        {
+        }
+        public DbSet<Npc> Npcs { get; set; }
+        public DbSet<Age> Ages { get; set; }
+        public DbSet<Faction> Factions { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Armor> Armors { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+        
+    }
+}
