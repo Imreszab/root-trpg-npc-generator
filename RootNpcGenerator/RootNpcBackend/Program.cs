299a8640e2370ace7e87f07e5c86060609e5c7a6
Imreszab <szabimi12@gmail.com>
2023-01-11 10:33:41 +0100

    add ApiHelperService

--- RootNpcGenerator/RootNpcBackend/Program.cs
+++ RootNpcGenerator/RootNpcBackend/Program.cs
@@ -1,5 +1,7 @@
 using RootNpcBackend.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.Extensions;
+using RootNpcBackend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,14 +11,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddDbContext<RootContext>(options =>
 {
-    var connectionstring = builder.Configuration.GetConnectionString("BadcampContext");
+    var connectionstring = builder.Configuration.GetConnectionString("RootContext") ?? throw new InvalidOperationException("Connetion string error");
     options.UseSqlServer(connectionstring);
 });
 builder.Services.AddTransient<RootNpcSeed>();
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var initialiser = services.GetRequiredService<RootNpcSeed>();
+//initialiser.Seed();    only run when no data in Db
+ApiHelperService.InitializeClient();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,6 +36,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder
+   .AllowAnyHeader()
+   .AllowAnyMethod()
+   .SetIsOriginAllowed((host) => true)
+   .AllowCredentials());
 
 app.UseHttpsRedirection();
 
