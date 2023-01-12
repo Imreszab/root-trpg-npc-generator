b30a27b0cef8415fc73c068c82b43f390303f884
Imreszab <szabimi12@gmail.com>
2023-01-12 09:58:30 +0100

    add cors service to builder

--- RootNpcGenerator/RootNpcBackend/Program.cs
+++ RootNpcGenerator/RootNpcBackend/Program.cs
@@ -6,11 +6,12 @@ using RootNpcBackend.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 
 builder.Services.AddDbContext<RootContext>(options =>
