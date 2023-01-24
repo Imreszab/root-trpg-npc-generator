88afbf28ce451c77d37ec3adde447378ce160c9f
Imreszab <szabimi12@gmail.com>
2023-01-24 11:19:30 +0100

    add Response and Guard

---
+++ RootNpcGenerator/RootNpcBackend/Services/Guard.cs
@@ -0,0 +1,11 @@
+﻿namespace RootNpcBackend.Services
+{
+    public static class Guard
+    {
+        public static void Require(bool precondition, string exceptionMessage)
+        {
+            if (!precondition)
+                throw new ArgumentException(exceptionMessage);
+        }
+    }
+}
